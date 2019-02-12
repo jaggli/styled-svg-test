a90d470d2d984d0c0d6252b8330f6a0cea1d6270
Matthias Jäggli <matthias.jaeggli@gmail.com>
2019-02-12 15:42:25 +0100

    initial

---
+++ src/Logo.test.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import Image from './Logo.js'
+
+describe('Logo.svg generated styled component', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallow(<Image />)
+  })
+
+  it('renders a <svg> tag without crashing', () => {
+    // expect(wrapper).toHaveTagName('svg')
+    // TODO: until this issue is resolved we have to use mount and find() https://github.com/styled-components/styled-components/issues/1985
+    expect(mount(<Image />).find('svg').length).toBe(1)
+  })
+
+  it('renders correctly according to snapshot', () => {
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('has dimensions greater than zero', () => {
+    const dimensions = Image.getDimensions()
+    expect(dimensions.width).not.toBe('0')
+    expect(parseInt(dimensions.width, 10)).toBeGreaterThan(0)
+    expect(dimensions.height).not.toBe('0')
+    expect(parseInt(dimensions.height, 10)).toBeGreaterThan(0)
+  })
+})
